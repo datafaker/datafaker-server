39d61fa9bd2005571efc5876703350c2c198fb6a
pnhung177 <pnhung177@gmail.com>
2021-04-26 11:31:49 +0700

    Rename the env.py to envars.py

--- src/env.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env python3
-
-ev.prefix = 'DATAFAKER'
---
+++ src/envars.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from configator.consts import ConstantMirror
+from configator.envars import EnvHelper, ev
+
+ev.prefix = 'DATAFAKER'
